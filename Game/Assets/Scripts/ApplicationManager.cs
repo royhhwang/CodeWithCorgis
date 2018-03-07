60504a6277ebb13757d5aa3cfc619d72cd45c82c
Roy Hwang <royhhwang@gmail.com>
2018-03-07 11:05:08 -0800

    reblox game + menu

---
+++ Game/Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ApplicationManager : MonoBehaviour
+{
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("royrebloxmenu");
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+		Application.Quit();
+#endif
+    }
+}
---
+++ Game/Assets/Scripts/WinPad.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinPad : MonoBehaviour {
+
+    public RollBox3 player;
+
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (player.isStanding)
+        {
+            Debug.Log("Win!");
+        }
+    }
+}
