c3d7d21d8e16ac37aa4c2ff03d6cc6a994763156
Jay Yang <hjyang1@uci.edu>
2018-03-07 19:42:04 -0800

    added results page

--- Game/Assets/Scripts/GameManager.cs
+++ Game/Assets/Scripts/GameManager.cs
@@ -7,31 +7,55 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
     public bool isGameOver;
     public bool isFalling;
+    public static bool showResults = false;
 
     public static int movesMade = 0;
     public Text moves;
     public Text timeLeft;
     public static float time = 99;
     public static int lvl = 1;
+    int numberOfLevels = 3;
+    bool resultsShown = false;
+
 
 	// Use this for initialization
 	void Start () {
         isFalling = false;
         isGameOver = false;
         moves.text = movesMade.ToString() + " moves made";
-
+        
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-        TimeTick();
+
+        if (!showResults)
+        {
+            if (time > 0)
+            {
+                TimeTick();
+            }
+            if (time <= 0.001)
+            {
+                showResults = true;
+                SceneManager.LoadScene("Results", LoadSceneMode.Single);
+            }
+
+        }
+
+        if (!resultsShown)
+        {
+            resultsShown = true;
+            ShowResultsText();       
+        }
+       
 	}
 
     public void UpMoveCounter ()
     {
         movesMade++;
         moves.text = movesMade.ToString() + " moves made";
-
     }
 
     void TimeTick()
@@ -43,12 +67,29 @@ public class GameManager : MonoBehaviour {
     public void LoadNextLevel()
     {
         lvl++;
-        SceneManager.LoadScene("Lvl "+lvl, LoadSceneMode.Single);
+        isGameOver = false;
+
+        if (lvl <= numberOfLevels)
+        {
+            SceneManager.LoadScene("Lvl " + lvl, LoadSceneMode.Single);
+        }
+        else
+        {
+            showResults = true;
+            SceneManager.LoadScene("Results", LoadSceneMode.Single);
+        }
+        
         
     }
 
     public void WaitThenLoad()
     {
         Invoke("LoadNextLevel", 2.0f);
+    }
+
+    void ShowResultsText ()
+    {
+        timeLeft.text = "Time Left: "+time.ToString("f2");
+        moves.text = "# of Moves Made: " + movesMade.ToString();
     }
 }
