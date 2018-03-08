4ab33e9945341b6f7216d57e1d095422e2be5574
Jay Yang <hjyang1@uci.edu>
2018-03-07 18:49:14 -0800

    added lvl 3

--- Game/Assets/Scripts/OffPlatform.cs
+++ Game/Assets/Scripts/OffPlatform.cs
@@ -7,16 +7,7 @@ public class OffPlatform : MonoBehaviour {
 
     public RollBox3 player;
     public GameManager gameManager;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    
 
     private void OnTriggerEnter(Collider other)
     {
