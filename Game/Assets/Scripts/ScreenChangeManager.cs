40bbd105ff7d05fe1659c892fd658d708e8ff53f
Roy Hwang <royhhwang@gmail.com>
2018-03-07 18:21:19 -0800

    stage transition animations

---
+++ Game/Assets/Scripts/ScreenChangeManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenChangeManager : MonoBehaviour
+{
+    public WinPad OnTriggerEnter;
+    Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        anim.Play("ScreenChange");
+    }
+}
--- Game/Assets/Scripts/WinPad.cs
+++ Game/Assets/Scripts/WinPad.cs
@@ -6,7 +6,12 @@ public class WinPad : MonoBehaviour {
 
     public RollBox3 player;
     public GameManager gameManager;
+    Animator anim;
 
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter(Collider collider)
     {
